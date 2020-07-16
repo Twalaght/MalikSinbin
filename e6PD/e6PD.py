aad3d02db8892d45554751517913e67adc52db70
Twalaght <jonathanchapman6799@gmail.com>
2020-07-17 01:20:34 +0930

    Minor e6PD changes

--- e6PD/e6PD.py
+++ e6PD/e6PD.py
@@ -5,47 +5,51 @@ import base64
 import configparser
 import time
 import urllib.request
+from pathlib import Path
 from requests import get
 
 # Read the arguments given to the script
 parser = argparse.ArgumentParser(description="Download a pool of images from e621")
-parser.add_argument("pool_id", metavar="Pool_ID", type=str, help="Pool of images to download (Ex: 11686)")
-parser.add_argument("name", metavar="Name", type=str, help="Name template to use when saving the images")
+parser.add_argument("pool_id", metavar="pool_ID", type=str, help="Pool of images to download (Ex: 11686)")
+parser.add_argument("name", metavar="name", type=str, help="Name template to use when saving the images")
 args = parser.parse_args()
 
-# Read the config file from the working directory
+# Read the config file from a specified location
 config = configparser.ConfigParser()
-config.read("config.txt")
+config.read(Path.home() / ".config" / "auths.txt")
 
-# TODO - Change this to be like tulkas
 # Set all relevant configuration variables
 user_agent = config.get("e621", "user_agent")
 user_name = config.get("e621", "user_name")
 API_key = config.get("e621", "API_key")
 
-# Calculate the basic auth b64 credential
+# Calculate the basic auth b64 credential and set the required request header
 user_auth = (user_name + ":" + API_key).encode("ascii")
 b64_auth = "Basic " + base64.b64encode(user_auth).decode("ascii")
-
-# Set the required header for all API requests
 headers = {"User-Agent": user_agent, "Authorization": b64_auth}
 
 # Get all the post IDs belonging to the pool
 pages = get("https://e621.net/pools/" + args.pool_id + ".json", headers=headers).json()["post_ids"]
 time.sleep(1.5)
 
+# Create the required folder with the given name
+Path(args.name).mkdir(parents=True, exist_ok=True)
+
 # Download each page in the pool
 for page in range(len(pages)):
-    # Status message
-    print("Parsing page " + str(page + 1) + "...")
-
+    # Print a status message for each page
+    print("Downloading page " + str(page + 1) + " of " + str(len(pages)) + "...")
+    
     # Get the source for each individual image
     image = get("https://e621.net/posts/" + str(pages[page]) + ".json", headers=headers).json()["post"]["file"]["url"]
     time.sleep(1.5)
 
-    # Get the file extension of the image
-    extension = "." + image.split(".")[-1]
+    # Generate the file path for each image to be saved to
+    img_path = Path.cwd() / args.name / (args.name + " " + str(page + 1).zfill(len(str(len(pages)))) + Path(image).suffix)
 
     # Write the image to disk
-    urllib.request.urlretrieve(image, args.name + " " + str(page + 1).zfill(len(str(len(pages)))) + extension)
+    urllib.request.urlretrieve(image, img_path) 
     time.sleep(1.5)
+
+# Print a final status when all downloads are finished
+print("Finished downloading " + args.name)
